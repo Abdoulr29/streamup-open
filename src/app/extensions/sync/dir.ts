2ab63bc9e9fc565ca17a3bf3d542e4c8a6450b80
richard457 <beastar457@gmail.com>
2017-04-01 17:32:40 +0200

    ....

--- src/app/extensions/sync/dir.ts
+++ src/app/extensions/sync/dir.ts
@@ -10,19 +10,21 @@ export class Mkdir {
     public create(dir: string): boolean {
 
         let newDir = os.homedir() + '/' + dir;
+        // os.homedir() + '/desktop' + dir;
         fs.exists(newDir, function (params, status) {
             // if (process.platform = 'linux') {
-                if (status !== true) {
-                    fs.mkdir(newDir, function (_, t) { });
-                    fs.chmod(newDir, '777', function (_, t) {
 
-                    });
+            if (status !== true) {
+                fs.mkdir(newDir, function (_, t) { });
+                fs.chmod(newDir, '777', function (_, t) {
 
-                } else {
-                    fs.chmod(newDir, '777', function (_, t) {
+                });
 
-                    });
-                }
+            } else {
+                fs.chmod(newDir, '777', function (_, t) {
+
+                });
+            }
             // } else if (process.platform = 'win32') {
             //     //please implement this here the logic 
             // }
